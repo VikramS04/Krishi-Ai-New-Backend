9304c6af4d641eeb1a2a4c4059f698398ecccd68
VikramS04 <vikramsaini7723@gmail.com>
2026-04-28 13:43:25 +0530

    Updated the db.js monogo error

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const communityRoutes = require('./routes/community')
 const app = express()
 
 // ─── Connect Database ─────────────────────────────────────────────────────────
-connectDB()
+await connectDB()
 
 // ─── Middleware ───────────────────────────────────────────────────────────────
 app.use(cors({ origin: '*', methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], allowedHeaders: ['Content-Type', 'Authorization'] }))
